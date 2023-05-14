f796a469eb2a5dc54243eb591166968bbe775cb8
Ross Cohen <rosscohen1990@gmail.com>
2023-05-14 19:44:59 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -587,7 +587,7 @@ export default function Home() {
 
               <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 items-center">
                 <span className="w-full text-center items-center rounded-md bg-blue-400/10 px-2 py-1 text-xs sm:text-sm md:text-md md:text-lg font-medium text-blue-400 ring-1 ring-inset ring-pink-blue/30">
-                  DOC CHATBOT
+                  AI Tutor PDF Reader
                 </span>
 
                 <div className="flex items-center gap-x-4 lg:gap-x-6">
